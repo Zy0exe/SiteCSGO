61f8d8ddefb48db89324a8a53d814355b9802f9f
Zy0.exe <zyoisbad@gmail.com>
2021-12-10 15:06:48 +0000

    im eating

--- js/snow.js
+++ js/snow.js
@@ -1,3 +1,4 @@
+// https://github.com/VincentGarreau/particles.js/
 document.addEventListener('DOMContentLoaded', function(){
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
